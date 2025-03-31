12b7e6dfd5589f18b2c68c0efbca0c1d8625fd54
harsh verma <harsh>
2025-03-31 16:11:20 +0530

    changes

--- src/AuthenticationPages/LoginPage/LoginPage1.jsx
+++ src/AuthenticationPages/LoginPage/LoginPage1.jsx
@@ -5,100 +5,91 @@ import UserState from "../../recoil/state/UserState";
 import LoginState from "../../recoil/state/LoginState";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import insta from "./insta.png"
-import linkedin from "./linkedin.png"
-import x from "./x.png"
-import youtube from "./youtube.png"
+import insta from "./insta.png";
+import linkedin from "./linkedin.png";
+import youtube from "./youtube.png";
 import toast from "react-hot-toast";
-import "./LoginPage.css"
-import logo3 from "./logo3.png"
+import "./LoginPage.css";
+import logo3 from "./logo3.png";
 import { Eye } from "lucide-react";
 import { EyeOff } from "lucide-react";
-import googleicons from "./Google_Icons.webp"
-import {useGoogleLogin} from "@react-oauth/google" 
+import googleicons from "./Google_Icons.webp";
+import { useGoogleLogin } from "@react-oauth/google";
 import { googleAuth } from "./api.js";
 
-
-
 const LoginPage1 = () => {
+  const navigate = useNavigate();
+  const [paswordVisibility, setpaswordVisibility] = useState(false);
 
-    const navigate = useNavigate();
-    const [paswordVisibility, setpaswordVisibility] = useState(false);
+  const [userCredentials, setusercredentials] = useRecoilState(UserState);
 
-    const [userCredentials , setusercredentials] = useRecoilState(UserState)
+  const [loginCredentials, setloginCredentials] = useRecoilState(LoginState);
 
-    const [loginCredentials, setloginCredentials] = useRecoilState(LoginState);
+  const handleCredentials = (event) => {
+    setloginCredentials((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
-    const handleCredentials = (event) => {
-        setloginCredentials((prev) => ({
-          ...prev,
-          [event.target.name]: event.target.value,
-        }));
-      };
+  const handleLogin = async (event) => {
+    event.preventDefault();
 
+    const { email, password } = loginCredentials;
 
-
-      const handleLogin = async (event) => {
-        event.preventDefault();
-        console.log("ihbshx");
-    
-        const { email, password } = loginCredentials;
-    
-        try {
-          const response = await axios.post(
-            'https://main-server-mu.vercel.app/login',
-            { email, password },
-            {
-              headers: { "Content-Type": "application/json" },
-              withCredentials: true,
-            }
-          );
-    
-          if(response.status === 200){
-            toast.success("Login SuccessFully!!!");
-            setusercredentials(response.data.user);
-            navigate("/")
-          }
-          else if([201,400].includes(response.status)){
-            toast.error(response.data.message)
-          }
-        } catch (error) {
-          console.log("Login failed ", error);
+    try {
+      const response = await axios.post(
+        "https://main-server-mu.vercel.app/login",
+        { email, password },
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
         }
-      };
+      );
 
-      const responseGoogle = async (authResult) => {
-        try {
-          if (!authResult?.code) {
-            throw new Error("No authorization code received.");
-          }
-          const result = await googleAuth(authResult.code);
-          if (result.status === 200) {
-            // const { email, firstname, lastname, username, token } = result.data.user;
-            // const user = { email, firstname, lastname, username, token };
-            setusercredentials(result.data.user)
-            toast.success("Login successful!");
-            navigate("/");
-          } else if(result.status === 201) {
-            toast.error(result.data.message)
-            navigate("/register")
-          }
-        } catch (error) {
-          console.error("Google Auth Error:", error);
-          toast.error(error.message || "Authentication failed");
-        }
-      };
-      
-      const googleLogin = useGoogleLogin({
-        onSuccess: (response) => responseGoogle(response),
-        onError: (error) => console.error("Google Login Error:", error),
-        flow: "auth-code",
-      });
+      if (response.status === 200) {
+        toast.success("Login SuccessFully!!!");
+        setusercredentials(response.data.user);
+        navigate("/");
+      } else if ([201, 400].includes(response.status)) {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log("Login failed ", error);
+    }
+  };
+
+  const responseGoogle = async (authResult) => {
+    try {
+      if (!authResult?.code) {
+        throw new Error("No authorization code received.");
+      }
+      const result = await googleAuth(authResult.code);
+      if (result.status === 200) {
+        // const { email, firstname, lastname, username, token } = result.data.user;
+        // const user = { email, firstname, lastname, username, token };
+        setusercredentials(result.data.user);
+        toast.success("Login successful!");
+        navigate("/");
+      } else if (result.status === 201) {
+        toast.error(result.data.message);
+        navigate("/register");
+      }
+    } catch (error) {
+      console.error("Google Auth Error:", error);
+      toast.error(error.message || "Authentication failed");
+    }
+  };
+
+  const googleLogin = useGoogleLogin({
+    onSuccess: (response) => responseGoogle(response),
+    onError: (error) => console.error("Google Login Error:", error),
+    flow: "auth-code",
+  });
 
   return (
     <div className="flex h-[630px]  bg-black items-center relative">
-
-          <div className="bg-grad-loginPage absolute left-[-300px]"></div>
+      <div className="bg-grad-loginPage absolute left-[-300px]"></div>
 
       <div className="flex w-full items-center gap-x-48 bg-black pl-12">
         <div className=" w-[54%] pt-5  items-end flex flex-col">
@@ -136,32 +127,27 @@ const LoginPage1 = () => {
               </label>
               <div className="relative">
                 <input
-                    className="w-[95%] px-2 py-2 rounded-xl bg-[#8FA0B1] text-black placeholder-black focus:outline-0"
-                    onChange={handleCredentials}
-                    type={paswordVisibility ? "text" : "password"}
-
-                    name="password"
-                    id="password"
-                    required
+                  className="w-[95%] px-2 py-2 rounded-xl bg-[#8FA0B1] text-black placeholder-black focus:outline-0"
+                  onChange={handleCredentials}
+                  type={paswordVisibility ? "text" : "password"}
+                  name="password"
+                  id="password"
+                  required
                 />
-                 <div
-                    onClick={() => setpaswordVisibility(!paswordVisibility)}
-                    className="absolute right-10 cursor-pointer   top-3"
-                  >
-                    {paswordVisibility ? (
-                      <Eye size={16} />
-                    ) : (
-                      <EyeOff size={16} />
-                    )}
-                  </div>
+                <div
+                  onClick={() => setpaswordVisibility(!paswordVisibility)}
+                  className="absolute right-10 cursor-pointer   top-3"
+                >
+                  {paswordVisibility ? <Eye size={16} /> : <EyeOff size={16} />}
+                </div>
               </div>
             </div>
-            <Link 
-  to="/forgot-password" 
-  className="font-outfit w-[95%] pr-2 text-[14px] font-normal pt-1 underline leading-[23.87px] text-right text-white hover:underline cursor-pointer"
->
-  Forgot Password
-</Link>
+            <Link
+              to="/forgot-password"
+              className="font-outfit w-[95%] pr-2 text-[14px] font-normal pt-1 underline leading-[23.87px] text-right text-white hover:underline cursor-pointer"
+            >
+              Forgot Password
+            </Link>
 
             <div className="justify-center item-center pt-4">
               <button
@@ -173,7 +159,12 @@ const LoginPage1 = () => {
             </div>
 
             <div className="flex gap-x-4 mt-3 justify-center pr-4">
-              <img onClick={googleLogin} className="w-8 cursor-pointer" src={googleicons} alt="" />
+              <img
+                onClick={googleLogin}
+                className="w-8 cursor-pointer"
+                src={googleicons}
+                alt=""
+              />
               <img className="w-8" src={insta} alt="" />
               <img className="w-8" src={youtube} alt="" />
               <img className="w-8" src={linkedin} alt="" />
@@ -207,7 +198,6 @@ const LoginPage1 = () => {
                   <p className="text-gray-500 text-sm">Designation</p>
                 </div>
               </div>
-
             </div>
           </div>
         </div>
